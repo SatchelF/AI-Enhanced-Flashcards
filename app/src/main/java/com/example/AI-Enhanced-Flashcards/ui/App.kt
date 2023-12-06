f8800d37d8b7f3bc87057f60e155853d1d98f5d5
SatchelF <satchcollege@gmail.com>
2023-12-05 18:40:32 -0700

    mess with themes

--- app/src/main/java/com/example/AI-Enhanced-Flashcards/ui/App.kt
+++ app/src/main/java/com/example/AI-Enhanced-Flashcards/ui/App.kt
@@ -2,11 +2,10 @@ package com.example.`AI-Enhanced-Flashcards`.ui
 
 import androidx.compose.runtime.Composable
 import com.example.`AI-Enhanced-Flashcards`.ui.navigation.RootNavigation
-import com.example.`AI-Enhanced-Flashcards`.ui.theme.CS3200FirebaseStarterTheme
+import com.example.`AI-Enhanced-Flashcards`.ui.theme.AIEnhancedFlashcardsTheme
 
 @Composable
 fun App() {
-    CS3200FirebaseStarterTheme {
         RootNavigation()
-    }
+
 }
